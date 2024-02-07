63730a5658ad3f273c436c452c467bcc2171ad8c
Devynn <158511151+DevynnGrey@users.noreply.github.com>
2024-02-07 10:54:52 -0800

    Updating page object model
    Added my @devynngrey/pageobject-helper page object module and updated the tests accordingly. Removed the current page object folder since it is now redundant.

--- cypress/e2e/basic-spec-test.cy.js
+++ cypress/e2e/basic-spec-test.cy.js
@@ -1,10 +1,10 @@
-import { pageObject } from "../pageObjects"
+import { nikePageObject } from "@devynngrey/pageobject-helper"
 
 describe('Basic Test Spec using NIKE.com', () => {
     it('Goes to the URL and validates an icon on the page', () => {
-      cy.visit(pageObject.url).then(() => {
+      cy.visit(nikePageObject.url).then(() => {
         cy.url().should('include', 'nike')
       })
-      cy.get(pageObject.homePageIcon).should('be.visible')
+      cy.get(nikePageObject.homePageIcon).should('be.visible')
     })
   })
